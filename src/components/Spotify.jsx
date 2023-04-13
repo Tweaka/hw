c525a1fccfd54b0b107be36acc8021e5f7c7083b
Tweaka <vzxpk@yahoo.fr>
2023-04-13 16:25:26 +0200

    supp home pas drole + spotify

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Spotify() {
+  return (
+	<div>
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src="https://open.spotify.com/embed/track/25H2jh84o1iYZrJvL3rU9q?utm_source=generator"  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
+	</div>
+  )
+}
